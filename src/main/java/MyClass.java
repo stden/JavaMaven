26174b1555ba8fe50f32018ca2b82dcc69176df4
Denis Stepulenok <super.denis@gmail.com>
2014-11-21 22:01:14 +0300

    Initial commit

---
+++ src/main/java/MyClass.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ *
+ */
+public class MyClass {
+    @Test
+    public void test() {
+        System.out.println("MyClass.test");
+    }
+}
---
+++ src/test/java/MyClassTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ *
+ */
+public class MyClassTest {
+
+    @Test
+    public void test1() {
+        System.out.println("MyClassTest.test1");
+    }
+}
