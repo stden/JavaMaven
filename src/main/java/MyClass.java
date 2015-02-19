d820ab8cc4ea67695660e11d8108a7a4f4d20636
Denis Stepulenok <super.denis@gmail.com>
2015-02-19 17:12:16 +0300

    Save changes

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -1,10 +1,7 @@
-import org.junit.Test;
-
 /**
  *
  */
 public class MyClass {
-    @Test
     public void test() {
         System.out.println("MyClass.test");
     }
--- src/test/java/MyClassTest.java
+++ src/test/java/MyClassTest.java
@@ -7,6 +7,7 @@ public class MyClassTest {
 
     @Test
     public void test1() {
+        //assertFalse(true);
         System.out.println("MyClassTest.test1");
     }
 }
